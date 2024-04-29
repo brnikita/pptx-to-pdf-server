db46266f4fd44703611c2bf032fc4990f6e97fd9
brnikita <branikita@gmail.com>
2024-04-29 21:03:14 +0300

    chore: Added dubugging mode for vscode

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,9 @@
 # Default values of arguments
 host="0.0.0.0"
 port="8000"
+debug_port="5678" # Debugging port for debugpy
 
-# Substitute environment variables in if provided
+# Substitute environment variables in, if provided
 if [ ! -z "$SERVER_HOST" ]; then
     host=$SERVER_HOST
 fi
@@ -14,5 +15,11 @@ if [ ! -z "$SERVER_PORT" ]; then
     port=$SERVER_PORT
 fi
 
-# Run uvicorn with the host and port parameters
-exec uvicorn app.main:app --host $host --port $port --reload
+# Debugging setup
+if [ "$DEBUG_MODE" = "true" ]; then
+    echo "Starting in debug mode on port $debug_port"
+    python -m debugpy --listen $host:$debug_port --wait-for-client -m uvicorn app.main:app --host $host --port $port --reload
+else
+    echo "Starting in normal mode"
+    exec uvicorn app.main:app --host $host --port $port --reload
+fi
